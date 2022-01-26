24972c0573a41a745f773e67ba78455e553dd757
Nate Good <nate.good@spiderstrategies.com>
2022-01-26 16:07:58 -0500

    fixed path to module

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const configReader = require('./config-reader')
-const findIssueNumber = require('find-issue-number')
+const findIssueNumber = require('./find-issue-number')
 
 module.exports = {
 	configReader,
