f732920ae39181f16e242bcf40313b9c6e186d93
Nate Good <nate.good@spiderstrategies.com>
2022-01-27 12:15:55 -0500

    Added base action, plus other refactoring (#1)

---
+++ base-action.js
@@ -0,0 +1,93 @@
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
+
+
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const dryRun = core.getInput('dry-run');
+const context = github.context;
+const { repository } = context.payload
+
+// Setup Octokit
+let octokit
+const repoToken = core.getInput('repo-token')
+if (repoToken) {
+	octokit = github.getOctokit(repoToken)
+}
+// If the event has a repository extract the attributes
+let repoOwnerParams = {}
+if (repository) {
+	repoOwnerParams = {
+		owner: repository.owner.login,
+		repo: repository.name
+	}
+}
+
+/**
+ * Common operations actions will invoke upon an instance of an action
+ */
+class BaseAction {
+
+	/**
+	 * Runs the action
+	 * @returns {Promise<void>}
+	 */
+	async run() {
+		return this.runAction().catch(err => {
+			core.error(err)
+			core.setFailed(err)
+		})
+	}
+
+	/**
+	 * Performs the action's work
+	 * @returns {Promise<void>}
+	 */
+	async runAction() {
+		throw new Error('Subclasses must implement runAction')
+	}
+
+	/**
+	 * Executes a command using FS.exec and performs logging and dry run logic.
+	 *
+	 * @param cmd
+	 *
+	 * @returns {Promise<string>}
+	 */
+	async exec(cmd) {
+		if (dryRun) {
+			core.info(`dry run: ${cmd}`)
+		} else {
+			core.info(`Running: ${cmd}`)
+			const { stdout, stderr } = await exec(cmd);
+			if (stderr) {
+				core.info(stderr)
+			}
+			return stdout.toString().trim()
+		}
+	}
+
+	/**
+	 * Make a GitHub API request using the octokit instance.
+	 * If the action did not specify a 'repo-token' input this will fail.
+	 *
+	 * @param {Function} apiFn (octokit.rest, opts) => {}
+	 * @param {Object} opts The options to apply to the api call in addition to the base options
+	 * @param {String} [label=''] Optionally label the operation in the log output
+	 * @returns {Promise<void>}
+	 */
+	async execRest(apiFn, opts, label = '') {
+		if (octokit && repoOwnerParams) {
+			const allOptions = {...repoOwnerParams, ...opts}
+			core.debug(`Invoking octokit rest api ${label}: ${JSON.stringify(opts)}`)
+			return await apiFn(octokit.rest, allOptions)
+		} else {
+			throw new Error(`octokit is not initialized! Did the action specify the required 'repo-token'?`)
+		}
+	}
+
+
+}
+
+module.exports = BaseAction
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const configReader = require('./config-reader')
 const findIssueNumber = require('./find-issue-number')
+const BaseAction = require('./base-action')
 
 module.exports = {
+	BaseAction,
 	configReader,
 	findIssueNumber
 }
