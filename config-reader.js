8926532ccdbb197573d3262b8c54595cee6f24b7
Nate Good <nate.good@spiderstrategies.com>
2022-02-01 10:11:12 -0500

    Added tests and new config attribute branchByAlias

--- config-reader.js
+++ config-reader.js
@@ -4,11 +4,25 @@ const { readFileSync } = require('fs')
  * @typedef Configuration
  *
  * @property {Object} config The config object as it was provided
+ * @property {Object} config.branches
+ * "branches": {
+ *     "release-2020-commercial": {
+ *       "alias": "2020",
+ *       "milestoneNumber": 1
+ *     },
+ *     ...
+ * }
  *
  * @property {Array<String>} mergeTargets An ordered array of branches that a
  * change should be merged forward into. (e.g. releases in chronological order)
  *
- * @property {Object} branchNameByMilestoneNumber e.g. { 1: 'release-2022' }
+ * @property {Object} branchNameByMilestoneNumber
+ * A mapping of milestoneNumber to the release branch the milestone is assigned to.
+ * e.g. { 1: 'release-2022' }
+ *
+ * @property {Object} branchByAlias
+ * A mapping of the branch alias to the branch object
+ * e.g. { '2021-sp': {branchAttributes...} }
  */
 
 /**
@@ -22,9 +36,20 @@ const { readFileSync } = require('fs')
 function configReader(configFileLocation, options = {}) {
 	const config = JSON.parse(readFileSync(configFileLocation))
 
+	const branchNameByMilestoneNumber = {}
+	const branchByAlias = {}
+
+	Object.entries(config.branches).forEach(entry => {
+		const [branchName, props] = entry;
+		const { alias, milestoneNumber } = props
+		branchNameByMilestoneNumber[milestoneNumber] = branchName
+		branchByAlias[alias] = { name: branchName, ...props }
+	})
+
 	const data = {
 		mergeTargets: buildMergeTargets(config, options),
-		branchNameByMilestoneNumber: branchNameByMilestone(config)
+		branchByAlias,
+		branchNameByMilestoneNumber
 	}
 
 	return {
@@ -44,33 +69,5 @@ function buildMergeTargets(config, options) {
 
 	return mergeTargets
 }
-
-/**
- * Get a mapping of milestonNumber to the release branch the milestone is assigned to.
- *
- * @param {Object} config.branches
- * "branches": {
- *     "release-2020-commercial": {
- *       "alias": "2020",
- *       "milestoneNumber": 1
- *     },
- *     ...
- * }
- *
- * @returns {Object}
- * {
- *     1: "release-2020-commercial"
- * }
- */
-function branchNameByMilestone(config) {
-	const returnValue = {}
-	Object.entries(config.branches).forEach(entry => {
-		const [branchName, props] = entry;
-		const { milestoneNumber } = props
-		returnValue[milestoneNumber] = branchName
-	})
-	return returnValue
-}
-
 
 module.exports = configReader
